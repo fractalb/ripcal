7e26bb22531188512bbbb924e550676b77a0c2f3
Balaji Marisetti <balajimarisetti@gmail.com>
2023-07-03 14:09:47 +0530

    Initial sources

---
+++ src/main.rs
@@ -0,0 +1,17 @@
+use std::env;
+use std::net::Ipv4Addr;
+use std::str::FromStr;
+
+fn main() {
+  let mut itr =  env::args();
+  println!("Program name: {:?}", itr.next().unwrap());
+
+  for a in itr {
+    if let Ok(addr) = Ipv4Addr::from_str(&a) {
+      let ip : u32 = addr.into();
+      println!("{} = {:#x}", a, ip);
+    } else {
+      println!("Invaid IP address: {}", a);
+    }
+  }
+}
