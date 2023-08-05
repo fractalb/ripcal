7427191d3c6c38cc1582c82b338dc194a048e20d
Balaji Marisetti <balajimarisetti@gmail.com>
2023-08-05 18:27:33 +0530

    Act as a filter command if no ip-address arguments are given

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,13 @@
 use std::env;
+use std::io::stdin;
+use std::io::BufRead;
 use std::iter::Iterator;
 use std::net::Ipv4Addr;
 use std::str::FromStr;
 
 const VERSION: &str = env!("CARGO_PKG_VERSION");
 const PKG_NAME: &str = env!("CARGO_PKG_NAME");
-const HELP_STR: &str = "ripcal [-i | -x | -q ] [-r] <ip-address>...\n\t\
+const HELP_STR: &str = "ripcal [-i | -x | -q ] [-r] [<ip-address>...]\n\t\
                                Converts each <ip-address> to different formats\n\t\
                                Options:\n\t\
                                --integer or -i\n\t\t\
@@ -16,6 +18,8 @@ const HELP_STR: &str = "ripcal [-i | -x | -q ] [-r] <ip-address>...\n\t\
                                        Converts to a ip-quad\n\t\
                                --reverse-bytes or -r\n\t\t\
                                        Reverse the byte order\n\n\
+                               If no ip-address arguments are given, then it'll\n\t\
+                               read from stdin and output to stdout (filter mode)\n\t\
                          \
                          ripcal -h or ripcal --help\n\t\
                                displays this help\n\n\
@@ -50,6 +54,7 @@ type OutputType = InputType;
 
 struct Config {
     reverse_bytes: bool,
+    filter_mode: bool,
     conversion_type: ConversionType,
 }
 
@@ -57,6 +62,7 @@ impl Config {
     fn default_config() -> Config {
         Config {
             reverse_bytes: false,
+            filter_mode: false,
             conversion_type: ConversionType::DefaultConversion,
         }
     }
@@ -101,14 +107,19 @@ fn main() {
         } else if a == "--help" || a == "-h" {
             return print_help();
         } else {
-            process_other_args(&mut env::args())
+            process_args(&mut env::args())
         }
+    } else {
+        // Enter filter mode.
+        // Read from stdin and print to stdout
+        process_stdin(Config::default_config());
     }
 }
 
-fn process_other_args(itr: &mut std::env::Args) -> () {
+fn process_args(itr: &mut std::env::Args) -> () {
     let mut config = Config::default_config();
     itr.next(); // Skip program name.
+    let mut empty_optional_args = true;
     for a in itr {
         if a == "--reverse-bytes" || a == "-r" {
             config.reverse_bytes = true;
@@ -118,49 +129,77 @@ fn process_other_args(itr: &mut std::env::Args) -> () {
             config.conversion_type = ConversionType::HexaDecimal;
         } else if a == "--ipv4" || a == "-q" {
             config.conversion_type = ConversionType::IpQuad;
-        } else if let Ok(addr) = Ipv4Addr::from_str(&a) {
-            // Dotted quad IPv4 address
-            let input_type = InputType::IpQuad;
-            let output_type = get_output_type(input_type, config.conversion_type);
-            let output = format_ipaddr(addr, output_type, config.reverse_bytes);
-            //println!("{} = {}", a, output);
-            println!(
-                "{}{} = {}",
-                if config.reverse_bytes { "Reverse " } else { "" },
-                a,
-                output
-            );
-        } else if let Some(a2) = a.strip_prefix("0x") {
-            // A hexadecimal number as IPv4 address
-            if let Ok(ip) = u32::from_str_radix(&a2, 16) {
-                let addr = Ipv4Addr::from(ip);
-                let input_type = InputType::HexaDecimal;
-                let output_type = get_output_type(input_type, config.conversion_type);
-                let output = format_ipaddr(addr, output_type, config.reverse_bytes);
-                println!(
-                    "{}{} = {}",
-                    if config.reverse_bytes { "Reverse " } else { "" },
-                    a,
-                    output
-                );
+        } else {
+            empty_optional_args = false;
+            process_ipaddress(&a, &config);
+        }
+    }
+
+    // Enter filter mode.
+    // Read from stdin and print to stdout
+    if empty_optional_args {
+        process_stdin(config);
+    }
+}
+
+fn process_stdin(config: Config) -> () {
+    let config = Config {
+        filter_mode: true,
+        ..config
+    };
+    let input = stdin();
+    for line in input.lock().lines() {
+        if let Ok(a) = line {
+            if a == "" {
+                println!("");
             } else {
-                println!("Invaid IP address: {}", a);
+                process_ipaddress(&a, &config);
             }
-        } else if let Ok(ip) = a.parse::<u32>() {
-            // A decimal number as IPv4 address
+        } else {
+            println!("Error reading input");
+        }
+    }
+}
+
+fn process_ipaddress(a: &str, config: &Config) -> () {
+    if let Ok(addr) = Ipv4Addr::from_str(&a) {
+        // Dotted quad IPv4 address
+        let input_type = InputType::IpQuad;
+        let output_type = get_output_type(input_type, config.conversion_type);
+        let output = format_ipaddr(addr, output_type, config.reverse_bytes);
+        print_output(&output, &a, &config);
+    } else if let Some(a2) = a.strip_prefix("0x") {
+        // A hexadecimal number as IPv4 address
+        if let Ok(ip) = u32::from_str_radix(&a2, 16) {
             let addr = Ipv4Addr::from(ip);
-            let input_type = InputType::DecaDecimal;
+            let input_type = InputType::HexaDecimal;
             let output_type = get_output_type(input_type, config.conversion_type);
             let output = format_ipaddr(addr, output_type, config.reverse_bytes);
-            //println!("{} = {}", a, output);
-            println!(
-                "{}{} = {}",
-                if config.reverse_bytes { "Reverse " } else { "" },
-                a,
-                output
-            );
+            print_output(&output, &a, &config);
         } else {
             println!("Invaid IP address: {}", a);
         }
+    } else if let Ok(ip) = a.parse::<u32>() {
+        // A decimal number as IPv4 address
+        let addr = Ipv4Addr::from(ip);
+        let input_type = InputType::DecaDecimal;
+        let output_type = get_output_type(input_type, config.conversion_type);
+        let output = format_ipaddr(addr, output_type, config.reverse_bytes);
+        print_output(&output, &a, &config);
+    } else {
+        println!("Invaid IP address: {}", a);
+    }
+}
+
+fn print_output(output: &str, input: &str, config: &Config) -> () {
+    if config.filter_mode {
+        println!("{}", output);
+    } else {
+        println!(
+            "{}{} = {}",
+            if config.reverse_bytes { "Reverse " } else { "" },
+            input,
+            output
+        );
     }
 }
